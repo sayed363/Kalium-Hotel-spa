20b160d93789dff56e93d3fa41822baea3830b7d
Mohammad Sayed <sayed.ttpl@gmail.com>
2019-07-22 22:15:42 +0600

    Accomodation page completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+
 $(document).ready(function(){
+	$('#navbarNav a').smoothScroll({
+		offset: -106, 		//set offset as per your need. Its custom.
+		speed:1000,		//1000 milisecond means 1 second
+	});
+	
+	// sticky navbar
+	$("#sticker").sticky({
+		topSpacing:0,
+		zIndex:9
+	
+	});
+	
+	
+	// masonry
+	var $grid = $('.filter-img').isotope({
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		masonry: {
+    // use outer width of grid-sizer for columnWidth
+		columnWidth: '1'
+		}
+	})
+	
+	// filter items on button click
+	$('.filter-button-group').on( 'click', 'button', function() {
+	  var filterValue = $(this).attr('data-filter');
+	  $grid.isotope({ filter: filterValue });
+	});
 	
 });
+
+
+
+
+
+$(window).on("scroll", function() {
+	if($(window).scrollTop()){
+		$('nav').addClass('.black');
+	}
+	else
+	{
+		$('nav').removeClass('.black');		
+	}
+})
